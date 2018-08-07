330b03d37b70ecf08f78ae42ddf63fcd3540178d
silence <imsilence@outlook.com>
2018-08-07 20:24:12 +0800

    add ch03
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ ch03/src/main/java/Annotation.java
@@ -0,0 +1,14 @@
+
+public class Annotation {
+    public static void main(String[] args) {
+        // 单行注释
+
+        /*
+            多行注释
+        */
+
+        /**
+        * 文档注释
+        */
+    }
+}
---
+++ ch03/src/main/java/DataType.java
@@ -0,0 +1,50 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        short height = 168;
+        int weight = 68;
+        long id = 1000L;
+        System.out.printf("id:%d, age: %d, height: %d, weight: %d\n", id, age, height, weight);
+
+        System.out.printf("byte min: %d, max: %d\n", Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("byte min: %d, max: %d\n", (byte)0X80, 0X7F);
+        System.out.printf("short min: %d, max: %d\n", Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("short min: %d, max: %d\n", (short)0X80_00, 0X7F_FF);
+        System.out.printf("int min: %d, max: %d\n", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("int min: %d, max: %d\n", 0X80_00_00_00, 0X7F_FF_FF_FF);
+        System.out.printf("long min: %d, max: %d\n", Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.printf("long min: %d, max: %d\n", 0X80_00_00_00_00_00_00_00L, 0X7F_FF_FF_FF_FF_FF_FF_FFL);
+
+        byte bsum = (byte)(age + age);
+        short ssum = (short)(height + height);
+        int isum = weight + weight;
+        long lsum = id + id;
+
+        System.out.printf("bsum: %d, ssum: %d, isum: %d, lsum: %d", bsum, ssum, isum, lsum);
+
+        float pi = 3.14F;
+        double money = 1000.31D;
+
+        System.out.printf("pi: %f\n", pi);
+        System.out.printf("money: %f\n", money);
+
+        System.out.printf("float min: %e, max: %e\n", Float.MIN_VALUE, Float.MAX_VALUE);
+        System.out.printf("double min: %e, max: %e\n", Double.MIN_VALUE, Double.MAX_VALUE);
+
+        float fsum = pi + pi;
+        double dsum = money + money;
+
+        System.out.printf("fsum: %f, dsum: %f\n", fsum, dsum);
+
+        char ch = 'A';
+        char registerNotation = '\u2122';
+        char doubleQuote = '"';
+
+        System.out.printf("a: %c, register notation: %c, double quote: %c\n", ch, registerNotation, doubleQuote);
+
+        boolean isBoy = true;
+
+        System.out.println(isBoy);
+    }
+}
